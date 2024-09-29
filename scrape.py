bba1b57588ee2bf1c78f6828854bd4843b829f58
Ryan Hermes <rryanhermes@gmail.com>
2024-09-28 23:23:22 -0500

    aggresive change

---
+++ scrape.py
@@ -0,0 +1,59 @@
+import base64
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part, SafetySetting
+
+
+def generate():
+    vertexai.init(project="personal-website-395618", location="us-central1")
+    model = GenerativeModel(
+        "gemini-1.5-flash-002",
+    )
+    responses = model.generate_content(
+        [text1, video1],
+        generation_config=generation_config,
+        safety_settings=safety_settings,
+        stream=True,
+    )
+
+    for response in responses:
+        print(response.text, end="")
+
+text1 = """Extract the ORGANIZATION NAME and WEBSITE from the table in the video into a JSON format. Ensure perfect accuracy with the url\'s. The JSON should look like this: [
+  {
+    \"Website\": \"https://example.com\"
+  },
+  {
+    \"Website\": \"https://another-example.com\"
+  }
+]"""
+video1 = Part.from_uri(
+    mime_type="video/*",
+    uri="https://youtu.be/ZUzCXwDmefY",
+)
+
+generation_config = {
+    "max_output_tokens": 8192,
+    "temperature": 1,
+    "top_p": 0.95,
+}
+
+safety_settings = [
+    SafetySetting(
+        category=SafetySetting.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold=SafetySetting.HarmBlockThreshold.OFF
+    ),
+    SafetySetting(
+        category=SafetySetting.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold=SafetySetting.HarmBlockThreshold.OFF
+    ),
+    SafetySetting(
+        category=SafetySetting.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold=SafetySetting.HarmBlockThreshold.OFF
+    ),
+    SafetySetting(
+        category=SafetySetting.HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold=SafetySetting.HarmBlockThreshold.OFF
+    ),
+]
+
+generate()
